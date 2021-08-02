aac73d75e4bcc0a29470500588424adcb1fdde99
Paul Batchelor <thisispaulbatchelor@gmail.com>
2021-08-02 11:35:18 -0400

    osc sndkit code updates

--- tangled/osc.c
+++ tangled/osc.c
@@ -4,11 +4,14 @@
 #include <math.h>
 #define SK_OSC_PRIV
 #include "osc.h"
-#line 105 "osc.org"
+#line 347 "osc.org"
+static float musl_rintf(float x);
+#line 44 "osc.org"
+#line 106 "osc.org"
 #define SK_OSC_MAXLEN 0x1000000L
 #define SK_OSC_PHASEMASK 0x0FFFFFFL
 #line 44 "osc.org"
-#line 92 "osc.org"
+#line 93 "osc.org"
 void sk_osc_freq(sk_osc *osc, SKFLT freq)
 {
     osc->freq = freq;
@@ -21,15 +24,15 @@ void sk_osc_amp(sk_osc *osc, SKFLT amp)
 #line 254 "osc.org"
 void sk_osc_init(sk_osc *osc, int sr, SKFLT *wt, int sz, SKFLT iphs)
 {
-#line 140 "osc.org"
+#line 141 "osc.org"
 osc->freq = 440;
 osc->amp = 0.2;
-#line 149 "osc.org"
+#line 150 "osc.org"
 osc->tab = wt;
 osc->sz = sz;
-#line 160 "osc.org"
+#line 161 "osc.org"
 osc->inc = 0;
-#line 170 "osc.org"
+#line 171 "osc.org"
 osc->lphs = ((int32_t)(iphs * SK_OSC_MAXLEN)) & SK_OSC_PHASEMASK;
 #line 205 "osc.org"
 {
@@ -60,25 +63,52 @@ SKFLT sk_osc_tick(sk_osc *osc)
 
     out = 0;
 #line 331 "osc.org"
-osc->inc = floor(lrintf(osc->freq * osc->maxlens));
+osc->inc = musl_rintf(osc->freq * osc->maxlens);
 #line 266 "osc.org"
-#line 342 "osc.org"
+#line 388 "osc.org"
 phs = osc->lphs;
 pos = phs >> osc->nlb;
 x1 = osc->tab[pos];
 x2 = osc->tab[(pos + 1) % osc->sz];
 #line 266 "osc.org"
-#line 372 "osc.org"
+#line 415 "osc.org"
 fract = (phs & osc->mask) * osc->inlb;
 #line 266 "osc.org"
-#line 397 "osc.org"
+#line 442 "osc.org"
 out = (x1 + (x2 - x1) * fract) * osc->amp;
 #line 266 "osc.org"
-#line 406 "osc.org"
+#line 451 "osc.org"
 phs += osc->inc;
 phs &= SK_OSC_PHASEMASK;
 osc->lphs = phs;
 #line 280 "osc.org"
     return out;
+}
+#line 352 "osc.org"
+/* ported from MUSL library. License: MIT */
+#define MUSL_FLT_EPSILON 1.1920928955078125e-07F
+#define MUSL_EPS MUSL_FLT_EPSILON
+static const float_t toint = 1/MUSL_EPS;
+static float musl_rintf(float x)
+{
+	int e;
+	int s;
+	float_t y;
+	union {float f; uint32_t i;} u;
+
+    u.f = x;
+
+	e = u.i>>23 & 0xff;
+	s = u.i>>31;
+
+	if (e >= 0x7f+23)
+		return x;
+	if (s)
+		y = x - toint + toint;
+	else
+		y = x + toint - toint;
+	if (y == 0)
+		return s ? -0.0f : 0.0f;
+	return y;
 }
 #line 44 "osc.org"
--- tangled/osc.h
+++ tangled/osc.h
@@ -4,13 +4,13 @@
 #ifndef SKFLT
 #define SKFLT float
 #endif
-#line 112 "osc.org"
+#line 113 "osc.org"
 typedef struct sk_osc sk_osc;
 #line 33 "osc.org"
 #ifdef SK_OSC_PRIV
-#line 116 "osc.org"
+#line 117 "osc.org"
 struct sk_osc {
-#line 123 "osc.org"
+#line 124 "osc.org"
 SKFLT freq, amp;
 SKFLT *tab;
 int inc;
@@ -20,15 +20,15 @@ SKFLT inlb;
 uint32_t mask;
 SKFLT maxlens;
 int32_t lphs;
-#line 118 "osc.org"
+#line 119 "osc.org"
 };
 #line 35 "osc.org"
 #endif
-#line 70 "osc.org"
+#line 71 "osc.org"
 void sk_osc_init(sk_osc *osc, int sr, SKFLT *wt, int sz, SKFLT iphs);
-#line 77 "osc.org"
+#line 78 "osc.org"
 SKFLT sk_osc_tick(sk_osc *osc);
-#line 86 "osc.org"
+#line 87 "osc.org"
 void sk_osc_freq(sk_osc *osc, SKFLT freq);
 void sk_osc_amp(sk_osc *osc, SKFLT amp);
 #line 37 "osc.org"
